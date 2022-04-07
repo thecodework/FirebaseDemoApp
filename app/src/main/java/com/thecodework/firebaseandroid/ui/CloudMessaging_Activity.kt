e8f968bfcb654b35da70f288d7515f876b7ad807
SonamAgrahari <sonamagrahari11@gmail.com>
2022-04-07 16:39:49 +0530

    implemented cloud Messaging

---
+++ app/src/main/java/com/thecodework/firebaseandroid/ui/CloudMessaging_Activity.kt
@@ -0,0 +1,46 @@
+package com.thecodework.firebaseandroid.ui
+
+import android.content.ContentValues.TAG
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.iid.FirebaseInstanceId
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.messaging.ktx.messaging
+import com.thecodework.firebaseandroid.R
+
+
+class CloudMessaging_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cloud_messaging)
+
+
+        Firebase.messaging.subscribeToTopic("weather")
+            .addOnCompleteListener { task ->
+                var msg = "Done"
+                if (!task.isSuccessful) {
+                    msg = "Failed"
+                }
+                Log.d(TAG, msg)
+                Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+            }
+
+       /* FirebaseInstanceId.getInstance().instanceId
+            .addOnCompleteListener(OnCompleteListener { task ->
+
+                if (!task.isSuccessful) {
+                    Log.w(TAG, "getInstanceId failed", task.exception)
+                    return@OnCompleteListener
+                }
+
+                val token = task.result?.token
+                val msg = token.toString()
+                Log.d(TAG, msg)
+                Toast.makeText(baseContext, msg, Toast.LENGTH_LONG).show()
+            })*/
+
+}
+}
