3921814a732062e35858e6f1b2c7f5c2447186a2
SonamAgrahari <sonamagrahari11@gmail.com>
2022-04-28 17:04:07 +0530

    completed student fragment screen

--- app/src/main/java/com/thecodework/firebaseandroid/ui/StudentFragment.kt
+++ app/src/main/java/com/thecodework/firebaseandroid/ui/StudentFragment.kt
@@ -1,15 +1,27 @@
 package com.thecodework.firebaseandroid.ui
 
+import android.app.Activity
+import android.content.ContentResolver
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import com.google.android.gms.tasks.OnFailureListener
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import com.thecodework.firebaseandroid.R
+import com.thecodework.firebaseandroid.databinding.ActivityCloudstorageBinding
 import com.thecodework.firebaseandroid.databinding.FragmentStudentBinding
+import java.io.IOException
+import java.util.*
+import kotlin.collections.HashMap
 
 class StudentFragment : Fragment() {
     private lateinit var binding: FragmentStudentBinding
@@ -17,6 +29,10 @@ class StudentFragment : Fragment() {
     lateinit var number: String
     lateinit var email: String
     lateinit var address: String
+    val PICK_IMAGE_REQUEST = 1
+    private var firebaseStore: FirebaseStorage? = null
+    private var storageReference: StorageReference? = null
+    private var filePath: Uri? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -35,11 +51,39 @@ class StudentFragment : Fragment() {
     }
 
     private fun initializer() {
-
+        firebaseStore = FirebaseStorage.getInstance()
+        storageReference = FirebaseStorage.getInstance().reference
     }
 
     private fun setClickListener() {
         binding.btnSave.setOnClickListener(View.OnClickListener {
+            binding.progress.visibility = View.VISIBLE
+            if (filePath != null) {
+                val ref = storageReference?.child("uploads/" + UUID.randomUUID().toString())
+                ref?.putFile(filePath!!)?.addOnSuccessListener { taskSnapshot ->
+                    taskSnapshot.storage.downloadUrl.addOnSuccessListener {
+                        binding.progress.visibility = View.GONE
+                        binding.imageProfile.setImageResource(R.drawable.ic_launcher_foreground)
+                        Toast.makeText(
+                            activity, "Save",
+                            Toast.LENGTH_LONG
+                        ).show()
+
+                    }
+                }
+
+                    ?.addOnFailureListener(OnFailureListener { e ->
+                        Toast.makeText(
+                            activity, e.message,
+                            Toast.LENGTH_LONG
+                        ).show()
+                        Log.d("TAG", e.message.toString())
+                    })
+
+
+            } else {
+                Toast.makeText(activity, "Please Upload an Image", Toast.LENGTH_SHORT).show()
+            }
             name = binding.edName.text.toString()
             number = binding.edNumber.text.toString()
             address = binding.edAddress.text.toString()
@@ -72,6 +116,33 @@ class StudentFragment : Fragment() {
 
 
         })
+        binding.tvUpload.setOnClickListener(View.OnClickListener {
+            val intent = Intent()
+            intent.type = "image/*"
+            intent.action = Intent.ACTION_GET_CONTENT
+            startActivityForResult(
+                Intent.createChooser(intent, "Select Picture"),
+                PICK_IMAGE_REQUEST
+            )
+        })
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK) {
+            if (data == null || data.data == null) {
+                return
+            }
+
+            filePath = data.data
+            try {
+                //  val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, filePath)
+                val contentResolver = requireActivity().contentResolver
+                val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, filePath)
+                binding.imageProfile.setImageBitmap(bitmap)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+    }
 }
