f291c9aa5bd17cb4c7ede7879d4f0400cf53589b
SonamAgrahari <sonamagrahari11@gmail.com>
2022-04-28 19:04:57 +0530

    added application class

--- app/src/main/java/com/thecodework/firebaseandroid/util/Application.kt
+++ app/src/main/java/com/thecodework/firebaseandroid/util/Application.kt
@@ -9,21 +9,16 @@ import com.google.firebase.messaging.ktx.messaging
 
 class Application : Application() {
 
-    companion object {
-        fun notification() {
-            Firebase.messaging.subscribeToTopic("weather")
-                .addOnCompleteListener { task ->
-                    var msg = "Done"
-                    if (!task.isSuccessful) {
-                        msg = "Failed"
-                    }
-                    Log.d(ContentValues.TAG, msg)
-                    //  Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
-                }
-        }
-    }
-
     override fun onCreate() {
         super.onCreate()
+        Firebase.messaging.subscribeToTopic("weather")
+            .addOnCompleteListener { task ->
+                var msg = "Done"
+                if (!task.isSuccessful) {
+                    msg = "Failed"
+                }
+                Log.d(ContentValues.TAG, msg)
+                Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+            }
     }
 }
